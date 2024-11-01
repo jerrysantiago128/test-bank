23f7616ee3e8813537861f8bbfab3ffc13fff908
jerrysantiago128 <144172168+jerrysantiago128@users.noreply.github.com>
2024-11-01 14:07:50 -0400

    Update BankSystem.java

--- java/BankSystem.java
+++ java/BankSystem.java
@@ -1,5 +1,31 @@
+import java.util.*;
 
-
-public class BankSystem{
+public class BankSystem  {
+	
+	//vars
+	private HashMap<String, Accounts> accountsMap;
+	
+	
+	//constructor
+	public BankSystem()
+	{
+		accountsMap = new HashMap<>();
+	}
+	
+	//create a new account
+	public void createAccount(String AccountNumber, String AccountHolderName, String AccountType, 
+			double initialDeposit, String currency ) {
+		if(!accountsMap.containsKey(AccountNumber)) {
+			Accounts newAccount = new Accounts(AccountNumber, AccountHolderName, AccountType, initialDeposit, currency );
+			accountsMap.put(AccountNumber, newAccount);
+			System.out.println("Account created Successfully");
+		}
+		else {
+			System.out.println("Account with this number already exists");   //the bank should give out an account number, not the user
+		}
+	}
+	
+	
+	
 	
 }
